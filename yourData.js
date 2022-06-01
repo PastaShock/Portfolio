7655a5f48faaa00b485196e25597d1b2b3e2b533
George Pastushok <gpastushok@yahoo.com>
2022-05-31 22:38:29 -0700

    initial commit

---
+++ yourData.js
@@ -0,0 +1,106 @@
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "George Pastushok",
+  headerTaglineOne: "Слава Україні!🇺🇦",
+  headerTaglineTwo:
+    `I'm George Pastushok - A full-stack developer in seattle.` ,
+
+  socials: [
+    { title: "Github", link: "https://github.com/pastashock" },
+    { title: "LinkedIn", link: "https://www.linkedin.com/in/george-pastushok-978621232/" },
+    { title: "Email", link: "mailto:georgepastushok@gmail.com" },
+  ],
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Cardiac Arrest", //Project Title - Add Your Project Title Here
+      description: "Run or bike on Hard mode",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+      "./assets/cardiacArrest.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/molano1979/project3",
+    },
+    {
+      title: "tech blog", //Project Title - Add Your Project Title Here
+      description: "A blog based on sequelize, express and handlebars",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+      "https://github.com/PastaShock/tech-blog/raw/main/assets/screenshot.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/PastaShock/tech-blog",
+    },
+    {
+      title: "social network api", //Project Title - Add Your Project Title Here
+      description: "An api using node, heroku, mongo, mongoose and express that has auth, users, sessions, posts and comments",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+      "./assets/social-network.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/PastaShock/social-network-api",
+    },
+    {
+      title: "Roland Print Automation", //Project Title - Add Your Project Title Here
+      description: "A Powershell, Nodejs and devtools project to automate my job as a print-pre-press operator",
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+      "./assets/roland.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/PastaShock/RolandPrintAutomation",
+    },
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        description: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // Services Section ---------------
+
+  services: [
+    {
+      title: "Front-end UX/UI",
+      description:
+        "Using React, and standard js based web design I create slick usable and light websites."
+    },
+    {
+      title: "Back-end Dev",
+      description:
+      "I have done many projects with MYSQL (sequelize as well as regular queries), MongoDb (cloud, local, mongoose) all through node js. I have done projects in nginx as well."
+    },
+    {
+      title: "Branding and design",
+      description:
+        "I've always been someone to like to create a good design. I have a knack for graphic design and have been working in illustrator for years. I made graphics for my church as a teenager and taught my sister how to use the adobe creative suite. Now shes a professional graphic designer.",
+    },
+  ],
+
+  // End Services Section --------------------------
+
+  // About Secton --------------
+  aboutpara:
+                `Hello,
+                I'm an aspiring developer delving into the deep dark depths of computer programming.
+                My first scripting language was HTML that I learned in a high school class taught by a basketball coach. Needless to say, I did not learn much and the curriculum was already far out of date.
+                Later I got into programming in C++ on arduinos making all sorts of odd contraptions.
+                But once I was out of community college I got a job and was far too busy with work to be able to have any projects like that.
+                later, in another job I found myself in a company that was rapidly growing. I had tasks that were repetitive and unnecessarily convoluted. So I set out to create scripts in VBA for Excel to make my workflow more efficient. From there I took that VB experience and translated it to Powershell to facilitate the organization of files and folders that everything I worked with would go. I sped up the searching of files by an order of magnitude, at least. In Powershell I developed skills that allowed me to hop into javascript. so you could say:
+                I have been fiddling with js.
+                In javascript I wrote scripts that manipulated a webpage to take over tasks that I would otherwise be clicking around in manually. I parsed information, made many conditional statements, used nested JS objects, created and exported JSON files to Powershell among other basic things.
+                When I developed my JS skills further, I used JSON in Powershell to take information from a webpage and make it usable in my prior scripts. I set up a lot of logging and tracking of my (regular day-to-day) work and, on the side, I've kept maintaining an updating code since`
+  //   End About Section ---------------------
+};
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
