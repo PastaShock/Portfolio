990da12fc113e2dc6ed8737ca7b79059393fe1ff
George Pastushok <gpastushok@yahoo.com>
2024-09-11 20:31:03 -0700

    removed nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import { useRef } from "react";
-import Link from "next/link";
 import Header from "../components/Header";
 import ServiceCard from "../components/ServiceCard";
 import Socials from "../components/Socials";
@@ -85,12 +84,6 @@ export default function Home() {
           <Socials />
         </div>
       </div>
-      <h1 className="text-sm text-bold mt-10 mob:mt-2 laptop:mt-10 mob:p-2 laptop:p-0">
-        Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
-        </Link>
-      </h1>
     </div>
   );
 }
